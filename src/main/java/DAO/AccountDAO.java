c73f1f9eaf22508f6eba396be35e8cf808c7c9e7
Jiahe Su <bluef0425@gmail.com>
2025-03-23 13:47:35 +0000

    Setting up files in detail

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,27 @@
 package DAO;
 
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AccountDAO {
-    
+    public Account insertAccount(Account account) {
+        // TODO: Write SQL to insert new account and return inserted Account with generated ID
+        return null;
+    }
+
+    public Account getAccountByUsername(String username) {
+        // TODO: Query DB for account with given username
+        return null;
+    }
+
+    public Account getAccountByUsernameAndPassword(String username, String password) {
+        // TODO: Query DB for account matching both username and password
+        return null;
+    }
+
 }
--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,40 @@
 package DAO;
 
+import Model.Message;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageDAO {
-    
+    public Message insertMessage(Message message) {
+        // Inserts a message into DB and returns the inserted Message (with generated ID)
+        return null;
+    }
+
+    public List<Message> getAllMessages() {
+        // Returns a list of all messages from DB
+        return null;
+    }
+
+    public Message getMessageById(int messageId) {
+        // Returns a specific message by ID
+        return null;
+    }
+
+    public Message deleteMessageById(int messageId) {
+        // Deletes a message by ID and returns the deleted Message if it existed
+        return null;
+    }
+
+    public Message updateMessage(int messageId, String newText) {
+        // Updates the message_text for a given ID and returns the updated Message
+        return null;
+    }
+
+    public List<Message> getMessagesByUser(int accountId) {
+        // Returns all messages posted by a specific account
+        return null;
+    }
 }
