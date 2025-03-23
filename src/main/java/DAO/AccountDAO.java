ef2af4368d9386d4b50f5845940feb358b8c3f5e
Jiahe Su <bluef0425@gmail.com>
2025-03-23 17:57:19 +0000

    accountdao1attempt

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -11,16 +11,69 @@ import java.sql.SQLException;
 public class AccountDAO {
     public Account insertAccount(Account account) {
         // TODO: Write SQL to insert new account and return inserted Account with generated ID
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?);" ;
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,account.getUsername());
+            preparedStatement.setString(2,account.getPassword());
+
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                ResultSet keys = preparedStatement.getGeneratedKeys();
+                if (keys.next()) {
+                    int id = keys.getInt(1);
+                    return new Account(id, account.getUsername(), account.getPassword());
+                }
+            }
+
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     public Account getAccountByUsername(String username) {
         // TODO: Query DB for account with given username
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username = ?;";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,username);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("account_id");
+                String password = rs.getString("password");
+                return new Account(id, username, password);
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     public Account getAccountByUsernameAndPassword(String username, String password) {
         // TODO: Query DB for account matching both username and password
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username = ? and password = ?;";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,password);
+            
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("account_id");
+                String pwd = rs.getString("password");
+                return new Account(id, username, pwd);
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
